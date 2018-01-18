65f001bf6e87d9bd9b11c70d88d14afb79828898
numajiri <numajiri@numa.local>
2018-01-18 11:35:32 +0900

    add bottle test

---
+++ python/bottle_server/server.py
@@ -0,0 +1,31 @@
+import pkg_resources
+import json
+from bottle import run, route, HTTPResponse
+
+
+def create_response(body):
+    r = HTTPResponse(status=200, body=body)
+    r.set_header('Content-Type', 'application/json')
+    return r
+
+
+@route("/")
+def index():
+    return pkg_resources.resource_string(__name__, "index.html")
+
+
+@route("/css/<file_name>")
+def css(file_name):
+    return pkg_resources.resource_string(__name__, "css/" + file_name)
+
+
+@route("/json_test")
+def json_test():
+    body = json.dumps({"name": "test"})
+    r = create_response(body)
+    return r
+
+
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=8080)
+
