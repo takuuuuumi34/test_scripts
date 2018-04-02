561fd60c16083c2568dbd99905e9976263f0d376
takuuuuumi34 <takuuuuumi.34@gmail.com>
2018-04-02 21:29:38 +0900

    add price from airbnb

--- python/selenium/run_selenium.py
+++ python/selenium/run_selenium.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
+options = Options()
+# required headless chrome.
+# options.add_argument('--headless')
+
+driver = webdriver.Chrome(chrome_options=options)
+
+url = "https://www.airbnb.jp/rooms/17113559"
+driver.get(url)
 print(driver.title)
 
-# take screenshot
-driver.save_screenshot("test.png")
+# class name for price element.
+elm = driver.find_element_by_class_name("_10cqp947")
+print(elm.text)
 driver.close()
+
